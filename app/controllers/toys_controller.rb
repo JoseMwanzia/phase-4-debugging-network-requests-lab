a2daf30ea6776827ee5d80c95abe3d3959991920
joseph mwanzia <makamwanzia@gmail.com>
2023-04-05 18:43:45 +0300

    Bug fixed for toy class

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,6 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 rescue_from ActiveRecord::RecordInvalid, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
   def index
     toys = Toy.all
@@ -8,7 +9,7 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_not_found
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
@@ -24,6 +25,10 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_not_found
   end
 
   private
+
+  def render_invalid
+    render json: {errors: errors.invalid.messages }, status: :unauthorized
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
